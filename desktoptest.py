a62e5cd2d98e51961a60e0882340b34e893af64e
Mitronics <65233828+Mitronics@users.noreply.github.com>
2020-06-01 17:06:57 +0200

    Add files via upload

--- desktoptest.py
+++ desktoptest.py
@@ -2,21 +2,7 @@ import RPi.GPIO as GPIO
 import time
 
 buttonState = None
-
-##  STATUSY
-global StatusDev
-StatusDev = {}
-StatusDev['Start'] = 0
-
-#0 - czeka na guzik
-#1 - START TESSTU
-StatusDev['currentState'] = 0
-StatusDev['reboot'] = 0
-StatusDev['ReceiveSNVer']=0
-    
-StatusDev['StartADBProcedure'] = 0
-StatusDev['ADBAnalize'] = 0
-StatusDev['TimeoutThanks'] = -2
+deviceState = None
 
 global buttons
 buttons ={}
@@ -41,28 +27,36 @@ buttons['START_press'] = 0   # GPIO_66
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(buttons['START'],GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(buttons['L83_S'], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(buttons['L83_S'], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(buttons['L83_P'], GPIO.OUT)
 
 while True:
-    print("Petla while")
+    
+    print("Poczatek petli while")
     time.sleep(0.2)
-    GPIO.output(buttons['L83_P'], GPIO.LOW)
-    buttonState = GPIO.input(buttons['START'])
-    print(buttonState)
-    GPIO.wait_for_edge(buttons['START'], GPIO.RISING) #oczekiwanie na wciśnięcie START
-    buttons['START_press'] = 1
-    if(StatusDev['Start']==0):
-        StatusDev['Start']=1
 
-    i2cConfButton['ButtonWatch']=0
     GPIO.output(buttons['L83_P'], GPIO.HIGH)
+
     buttonState = GPIO.input(buttons['START'])
-    print(buttonState)
-    channel = GPIO.wait_for_edge(buttons['L83_S'], GPIO.RISING, timeout=20000) #oczekiwanie na banknot
+    print('Stan przycisku przed nacisnieciem: ' + str(buttonState))
+
+    GPIO.wait_for_edge(buttons['START'], GPIO.FALLING, bouncetime = 300) #oczekiwanie na wciśnięcie START
+
+    buttonState = GPIO.input(buttons['START'])
+    GPIO.output(buttons['L83_P'], GPIO.LOW)
+    print('Stan przycisku po nacisnieciu: ' + str(buttonState))
+    time.sleep(0.5)
+    buttonState = GPIO.input(buttons['START'])
+    print('Stan przycisku 0.5 sekundy po nacisnieciu: ' + str(buttonState))
+
+    deviceState = GPIO.input(buttons['L83_S'])
+    print('Stan akceptora przed podaniem banknotu: ' + str(deviceState))
+    
+    channel = GPIO.wait_for_edge(buttons['L83_S'], GPIO.RISING, timeout=20000, bouncetime = 300) #oczekiwanie na banknot
+
     if channel is None:
-        SlupekGUI.BackToStart()
+        print('timeout')
     else:
-        if(StatusDev['currentState']==0):
-            StatusDev['currentState']=1
-            time.sleep(4)
+        deviceState = GPIO.input(buttons['L83_S'])
+        print('Stan akceptora po podaniu banknotu: ' + str(deviceState))
+
